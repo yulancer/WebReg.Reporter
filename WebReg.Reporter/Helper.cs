3eb03e7f787d5727605503a9f0319ced0932dd62
Юрий Матвеев <yulancer@yandex.ru>
2022-08-12 18:10:00 +0300

    все типы отчётов авторегистрируются в контейнере

--- WebReg.Reporter/Helper.cs
+++ WebReg.Reporter/Helper.cs
@@ -1,4 +1,6 @@
-﻿using WebReg.Reporter.Adapters.TemplateEngine;
+﻿using System.Reflection;
+
+using WebReg.Reporter.Adapters.TemplateEngine;
 using WebReg.Reporter.Data.Implementations.Services;
 using WebReg.Reporter.Domain.Contracts.Interfaces;
 using WebReg.Reporter.Domain.Implementations.Services;
@@ -17,6 +19,32 @@ public static class Helper
         services.AddScoped<IReportRepository, ReportRepository>();
         services.AddScoped<ITemplateEngine, FluidTemplateEngine>();
         services.AddScoped<ISender, Sender>();
+
+        services.AddAllReports();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAllReports(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetEntryAssembly();
+        if (assembly != null)
+        {
+            var assemblies = assembly.GetReferencedAssemblies();
+
+            foreach (var assemblyName in assemblies)
+            {
+                assembly = Assembly.Load(assemblyName);
+
+                foreach (var ti in assembly.DefinedTypes)
+                {
+                    if (ti.ImplementedInterfaces.Contains(typeof(IReport)))
+                    {
+                        services.AddTransient(typeof(IReport), ti);
+                    }
+                }
+            }
+        }
 
         return services;
     }
